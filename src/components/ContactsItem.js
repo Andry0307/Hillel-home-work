0360e5eedd7f297cd6095174e2236fae60905848
Andry <andry03071993@gmail.com>
2020-01-23 14:22:45 +0200

    push

---
+++ src/components/ContactsItem.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class ContactsItem extends Component {
+    render() {
+        const {name, surname, phone, age} = this.props.item;
+        return (
+            <div>
+                <li className='App-list'>
+                    <p>name: {name} {surname}</p>
+                    <p>age: {age}</p>
+                    <p>phone: {phone}</p>
+                </li>
+            </div>
+        );
+    }
+}
+
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,21 +1,16 @@
 import React, {Component} from 'react';
+import ContactsItem from "./ContactsItem";
 
 export default class ContactsList extends Component {
 
     render() {
         const {contacts} = this.props;
         return (
-            <div>
                 <ul>
-                {contacts.map((contact) =>
-                    <li className='App-list' key={contact.id}>
-                        <p>name: {contact.name} {contact.surname}</p>
-                        <p>age: {contact.age}</p>
-                        <p>phone: {contact.phone}</p>
-                    </li>
+                {contacts.map((itemContacts) =>
+                    <ContactsItem key={itemContacts.id} item={itemContacts}/>
                 )}
                 </ul>
-            </div>
 
         );
     }
